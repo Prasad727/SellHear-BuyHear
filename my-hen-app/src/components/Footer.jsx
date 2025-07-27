3a13b0daaf57fa79556c28f157d3c2a5cc8824ff
Lakshmiprasad Putti <117511904+Prasad727@users.noreply.github.com>
2025-07-27 22:04:14 +0530

    Update Footer.jsx

--- my-hen-app/src/components/Footer.jsx
+++ my-hen-app/src/components/Footer.jsx
@@ -7,25 +7,45 @@ const Footer = () => {
       <div className="container">
         <div className="row">
 
-          {/* ℹ️ Brand Info */}
+          {/* Brand Info */}
           <div className="col-md-4 mb-3">
             <h5 className="text-warning">SellHear-BuyHear.in</h5>
             <p>Your trusted platform to sell and buy livestock easily, securely, and quickly.</p>
           </div>
 
-          {/* 🔗 Quick Links */}
+          {/* Quick Links */}
           <div className="col-md-4 mb-3">
             <h6>Quick Links</h6>
             <ul className="list-unstyled">
-              <li><Link to="/" className="text-light text-decoration-none">Home</Link></li>
-              <li><Link to="/punju" className="text-light text-decoration-none">Roosters</Link></li>
-              <li><Link to="/goat" className="text-light text-decoration-none">Goats</Link></li>
-              <li><Link to="/buffalo" className="text-light text-decoration-none">Buffaloes</Link></li>
-              <li><Link to="/login" className="text-light text-decoration-none">Login</Link></li>
+              <li>
+                <Link to="/" className="text-light text-decoration-none">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link to="/punju" className="text-light text-decoration-none">
+                  Roosters
+                </Link>
+              </li>
+              <li>
+                <Link to="/goat" className="text-light text-decoration-none">
+                  Goats
+                </Link>
+              </li>
+              <li>
+                <Link to="/buffalo" className="text-light text-decoration-none">
+                  Buffaloes
+                </Link>
+              </li>
+              <li>
+                <Link to="/login" className="text-light text-decoration-none">
+                  Login
+                </Link>
+              </li>
             </ul>
           </div>
 
-          {/* 📞 Contact Info */}
+          {/* Contact Info */}
           <div className="col-md-4 mb-3">
             <h6>Contact Us</h6>
             <p className="mb-1">Email: sellhearbuyhear@gmail.com</p>
@@ -35,6 +55,7 @@ const Footer = () => {
         </div>
 
         <hr className="border-secondary" />
+
         <p className="text-center small mb-0">
           &copy; {new Date().getFullYear()} SellHear-BuyHear.in. All rights reserved.
         </p>
